2dfb7defe561381ed4ee7ab3eff359d7d47ef3a3
Carlos Ruiz Vasquez <cyrv_2@hotmail.com>
2016-12-10 11:55:13 -0500

    EC2

--- apps/inicio/admin.py
+++ apps/inicio/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from .models import Modulo, Menu
 # Register your models here.
 
-class AdminModulo(admin.ModelAdmin):
-	list_display_links = ['id','nombre','orden']
-	list_editable = ['id','nombre', 'orden']
+# class AdminModulo(admin.ModelAdmin):
+# 	list_display_links = ['id','nombre','orden']
+# 	list_editable = ['id','nombre', 'orden']
 
-class AdminMenu(admin.ModelAdmin):
+# class AdminMenu(admin.ModelAdmin):
 
-	list_display_links = ['id','nombre','iconoclase','modulo','menupadre','orden','control']
-	list_editable = ['id','nombre','iconoclase','modulo','menupadre','orden','control']
+# 	list_display_links = ['id','nombre','iconoclase','modulo','menupadre','orden','control']
+# 	list_editable = ['id','nombre','iconoclase','modulo','menupadre','orden','control']
 
 admin.site.register(Modulo, AdminModulo)
 admin.site.register(Menu, AdminMenu)
